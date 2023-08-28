52037238fd79c60bea5ca57a8a704e91c9806c57
Fatma2645 <142684037+Fatma2645@users.noreply.github.com>
2023-08-28 09:58:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# write your code here
+# write your code
+favourite_anamals=["cat","dog","monkey","rabbit"]
+print(favourite_anamals [2])
+# favourite_anamals.remove(3)
+
+
+
+# favourite_anamals.append(lion)
+for oneAnamil in favourite_anamals:
+    print(f"I love {oneAnamil}")
+
+
+
+numbers=[1,2,3,4,5]
+numbers_sum= 0
+for onenumber in numbers:
+    numbers_sum = onenumber + numbers_sum
+print(numbers_sum)
